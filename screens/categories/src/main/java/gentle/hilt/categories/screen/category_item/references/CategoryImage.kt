2f9e20b8f97f20d8a740b5420e665615ace27e8b
Gentle_Hilt <nindzyago61@gmail.com>
2023-11-30 00:46:49 +0500

    Crossfade effect on image loading using openBuilderAsyncImagePointer()

--- screens/categories/src/main/java/gentle/hilt/categories/screen/category_item/references/CategoryImage.kt
+++ screens/categories/src/main/java/gentle/hilt/categories/screen/category_item/references/CategoryImage.kt
@@ -4,13 +4,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import coil.compose.rememberAsyncImagePainter
-
+import gentle.hilt.data.res.custom_composables.openBuilderAsyncImagePainter
 
 @Composable
 fun CategoryImage(image: String?, modifier: Modifier) {
     Image(
-        painter = rememberAsyncImagePainter(model = image),
+        painter = openBuilderAsyncImagePainter(image, builder = { crossfade(true) }),
         contentDescription = null,
         modifier = modifier,
         contentScale = ContentScale.Crop
