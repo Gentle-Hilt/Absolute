3c0bcb152f61fcb21eb979c493886efd5fc37969
Gentle_Hilt <nindzyago61@gmail.com>
2023-11-30 00:51:23 +0500

    Fonts, FontSizes.

---
+++ data/src/main/java/gentle/hilt/data/res/font/FontSizes.kt
@@ -0,0 +1,6 @@
+package gentle.hilt.data.res.font
+
+object FontSizes{
+    const val titlesFont = 18
+    const val textFont = 15
+}
---
+++ data/src/main/java/gentle/hilt/data/res/font/Fonts.kt
@@ -0,0 +1,9 @@
+package gentle.hilt.data.res.font
+
+import android.graphics.Typeface
+import androidx.compose.ui.text.font.FontFamily
+
+
+object Fonts {
+    val sans_serif = FontFamily(Typeface.SANS_SERIF)
+}
